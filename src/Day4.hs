69d3a64635e708474d0c4ad35de2b47d59bfc7e3
hackeryarn <artemchernyak@gmail.com>
2020-12-12 21:35:51 -0600

    Finish day 4

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 -- | Passport Processing
 
 module Day4 where
@@ -8,38 +9,112 @@ import qualified Data.Text.IO                  as T
 import           Data.Either
 import           Text.Parsec
 import           Text.Parsec.Perm
+import           Text.Parsec.Text
 
 data Passport = Passport
-  { passportBirthYear      :: String
-  , passportIssueYear      :: String
-  , passportExpirationYear :: String
-  , passportHeight         :: String
-  , passportHairColor      :: String
-  , passportEyeColor       :: String
-  , passportID             :: String
+  { passportBirthYear      :: Year
+  , passportIssueYear      :: Year
+  , passportExpirationYear :: Year
+  , passportHeight         :: Height
+  , passportHairColor      :: HairColor
+  , passportEyeColor       :: EyeColor
+  , passportID             :: PassportId
   , passportCountryID      :: Maybe String
   }
   deriving (Show, Eq)
 
-parsePassportField :: Parsec T.Text st String
-parsePassportField = many (alphaNum <|> char '#') <* skipMany space
+newtype Year = Year Int
+  deriving (Show, Eq)
 
-parsePassport :: Parsec T.Text st Passport
+data Height = Height
+  { heightNum     :: Int
+  , heightMeasure :: String
+  }
+  deriving (Show, Eq)
+
+newtype HairColor = HairColor String
+  deriving (Show, Eq)
+
+newtype EyeColor = EyeColor String
+  deriving (Show, Eq)
+
+newtype PassportId = PassportId Int
+  deriving (Show, Eq)
+
+parsePassport :: Parser Passport
 parsePassport =
   permute
     $    Passport
-    <$$> try (string "byr:" *> parsePassportField)
-    <||> try (string "iyr:" *> parsePassportField)
-    <||> try (string "eyr:" *> parsePassportField)
-    <||> try (string "hgt:" *> parsePassportField)
-    <||> try (string "hcl:" *> parsePassportField)
-    <||> try (string "ecl:" *> parsePassportField)
-    <||> try (string "pid:" *> parsePassportField)
+    <$$> try (string "byr:" *> parseYear)
+    <||> try (string "iyr:" *> parseYear)
+    <||> try (string "eyr:" *> parseYear)
+    <||> try (string "hgt:" *> parseHeight)
+    <||> try (string "hcl:" *> parseHairColor)
+    <||> try (string "ecl:" *> parseEyeColor)
+    <||> try (string "pid:" *> parsePassportId)
     <|?> (Nothing, fmap Just (try (string "cid:" *> parsePassportField)))
+
+parsePassportField :: Parser String
+parsePassportField = many (alphaNum <|> char '#') <* skipMany space
+
+parseYear :: Parser Year
+parseYear = Year . read <$> count 4 digit <* skipMany space
+
+parseHeight :: Parser Height
+parseHeight = do
+  heightNum     <- read <$> many1 digit
+  heightMeasure <- count 2 letter
+  skipMany space
+  pure $ Height { .. }
+
+parseHairColor :: Parser HairColor
+parseHairColor =
+  HairColor
+    <$> (  char '#'
+        *> count 6 (digit <|> oneOf ['a', 'b', 'c', 'd', 'e', 'f'])
+        <* skipMany space
+        )
+
+parseEyeColor :: Parser EyeColor
+parseEyeColor =
+  EyeColor
+    <$> (   try (string "amb")
+        <|> try (string "blu")
+        <|> try (string "brn")
+        <|> try (string "gry")
+        <|> try (string "grn")
+        <|> try (string "hzl")
+        <|> try (string "oth")
+        )
+    <*  skipMany space
+
+parsePassportId :: Parser PassportId
+parsePassportId = PassportId . read <$> count 9 digit <* skipMany space
+
+validateBirthYear :: Year -> Bool
+validateBirthYear (Year by) = by >= 1920 && by <= 2002
+
+validateIssueYear :: Year -> Bool
+validateIssueYear (Year iy) = iy >= 2010 && iy <= 2020
+
+validateExpirationYear :: Year -> Bool
+validateExpirationYear (Year ey) = ey >= 2020 && ey <= 2030
+
+validateHeight :: Height -> Bool
+validateHeight (Height cm     "cm") = cm >= 150 && cm <= 193
+validateHeight (Height inches "in") = inches >= 59 && inches <= 76
+validateHeight _                    = False
+
+validatePassport :: Passport -> Bool
+validatePassport Passport {..} =
+  validateBirthYear passportBirthYear
+    && validateIssueYear passportIssueYear
+    && validateExpirationYear passportExpirationYear
+    && validateHeight passportHeight
 
 solve :: IO ()
 solve = do
   let fname = "input/day4.txt"
   input <- T.readFile fname
   let passports = parse parsePassport fname <$> T.splitOn "\n\n" input
-  print . length . filter isRight $ passports
+  print . length . filter validatePassport . rights $ passports
--- test/Day4Test.hs
+++ test/Day4Test.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 module Day4Test where
 
 import qualified Data.Text                     as T
@@ -6,58 +5,38 @@ import           Test.Hspec
 import           Text.Parsec
 import           Day4
 
+isValidPassport :: Either pe Passport -> Bool
+isValidPassport (Right _) = True
+isValidPassport (Left  _) = False
+
 day4 :: Spec
 day4 = do
   describe "parsePassport" $ do
     let
       passport =
         T.pack
-          "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm"
-    it "parses passport with all fields"
-      $          parse parsePassport "" passport
-      `shouldBe` Right
-                   (Passport { passportBirthYear      = 1937
-                             , passportIssueYear      = 2017
-                             , passportExpirationYear = 2020
-                             , passportHeight         = "183cm"
-                             , passportHairColor      = "#fffffd"
-                             , passportEyeColor       = "gry"
-                             , passportID             = 860033327
-                             , passportCountryID      = Just 147
-                             }
-                   )
+          "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f"
+    it "parses valid passport 1"
+      $               parse parsePassport "" passport
+      `shouldSatisfy` isValidPassport
     let
       passport =
         T.pack
-          "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm"
-    it "parses passport with missing cid"
-      $          parse parsePassport "" passport
-      `shouldBe` Right
-                   (Passport { passportBirthYear      = 1931
-                             , passportIssueYear      = 2013
-                             , passportExpirationYear = 2024
-                             , passportHeight         = "179cm"
-                             , passportHairColor      = "#ae17e1"
-                             , passportEyeColor       = "brn"
-                             , passportID             = 760753108
-                             , passportCountryID      = Nothing
-                             }
-                   )
+          "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm"
+    it "parses valid passport 2"
+      $               parse parsePassport "" passport
+      `shouldSatisfy` isValidPassport
     let
       passport =
         T.pack
-          "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929"
-    it "does not parse passport with missing fields"
+          "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022"
+    it "parses valid passport 3"
       $               parse parsePassport "" passport
-      `shouldSatisfy` (\case
-                        (Left _)  -> True
-                        otherwise -> False
-                      )
-    let passport =
-          T.pack "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in"
-    it "does not parse another invalid passport"
+      `shouldSatisfy` isValidPassport
+    let
+      passport =
+        T.pack
+          "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+    it "parses valid passport 4"
       $               parse parsePassport "" passport
-      `shouldSatisfy` (\case
-                        (Left _)  -> True
-                        otherwise -> False
-                      )
+      `shouldSatisfy` isValidPassport
